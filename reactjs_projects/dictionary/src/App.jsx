ee9e4f399fb482ebd0ee56305c703ad9e436dd2a
Sahil Mahapatra <sahilm231199@gmail.com>
2023-12-10 14:25:52 +0530

    added functionality to search button to print to console

--- reactjs_projects/dictionary/src/App.jsx
+++ reactjs_projects/dictionary/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Noun from "./components/Noun";
@@ -6,10 +7,19 @@ import SearchWord from "./components/SearchWord";
 import Verb from "./components/Verb";
 
 export default function App() {
+  const [searchWord, setSearchWord] = useState("");
+
+  function handleSearchWordChange(event) {
+    setSearchWord(event.target.value);
+  }
+
   return (
     <div className="mx-96 my-10">
       <Navbar />
-      <SearchBar />
+      <SearchBar
+        onChange={(e) => handleSearchWordChange(e)}
+        searchWord={searchWord}
+      />
       <SearchWord />
       <Noun />
       <Verb />
--- reactjs_projects/dictionary/src/components/SearchBar.jsx
+++ reactjs_projects/dictionary/src/components/SearchBar.jsx
@@ -1,12 +1,19 @@
-function SearchBar() {
+function SearchBar({ onChange, searchWord }) {
+  function handleWordSubmit(event) {
+    event.preventDefault();
+    console.log(searchWord);
+  }
+
   return (
     <form className="my-10 border-none rounded-xl flex justify-between py-3 px-6 bg-white-dark">
       <input
         className="w-full border-none focus:ring-0 bg-white-dark text-black-darkest"
         placeholder="Learn Something new..."
         type="text"
+        value={searchWord}
+        onChange={onChange}
       />
-      <button>
+      <button onClick={(e) => handleWordSubmit(e)}>
         <img src="/assets/images/icon-search.svg" />
       </button>
     </form>
