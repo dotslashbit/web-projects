2a2c9600e4aa7075789d41130e5a611b99038c9e
Sahil Mahapatra <sahilm231199@gmail.com>
2023-12-10 11:33:47 +0530

    finished getting a rough static version of the design and also added custom color palette

--- reactjs_projects/dictionary/src/App.jsx
+++ reactjs_projects/dictionary/src/App.jsx
@@ -1,7 +1,69 @@
 export default function App() {
   return (
-    <div>
-      <h1 class="italic">Hello World!</h1>
+    <div className="mx-96 my-10">
+      <nav className="flex justify-between">
+        <img src="assets/images/logo.svg" />
+        <div className="flex justify-between">
+          <p>Sans Sarif</p>
+          <p>Theme Switch</p>
+        </div>
+      </nav>
+      <form className="my-10 border-solid border-2 border-sky-500 rounded-xl flex justify-between p-4">
+        <input placeholder="Learn Something new..." type="text" />
+        <button>
+          <img src="/assets/images/icon-search.svg" />
+        </button>
+      </form>
+      <main>
+        <section className="word flex justify-between my-10">
+          <div className="flex flex-col justify-between items-start gap-4">
+            <h2 className="text-6xl font-bold">Keyboard</h2>
+            <p className="text-2xl font-light text-violet">/ˈkiːbɔːd/</p>
+          </div>
+          <img src="assets/images/icon-play.svg" />
+        </section>
+        <section className="noun">
+          <p className="italic font-bold text-xl">noun</p>
+          <p className="meaning text-white-darkest text-xl font-light my-5">
+            Meaning
+          </p>
+          <ul className="meaning-list ml-10 list-disc marker:text-violet">
+            <li className="my-4">
+              (etc.) A set of keys used to operate a typewriter, computer etc.
+            </li>
+            <li className="my-4">
+              A component of many instruments including the piano, organ, and
+              harpsichord consisting of usually black and white keys that cause
+              different tones to be produced when struck.
+            </li>
+            <li className="my-4">
+              A device with keys of a musical keyboard, used to control
+              electronic sound-producing devices which may be built into or
+              separate from the keyboard device.
+            </li>
+          </ul>
+          <div className="synonyms flex justify-start gap-10">
+            <p>Synonyms</p>
+            <p className="text-violet">Electronic Keyboard</p>
+          </div>
+        </section>
+        <section className="verb">
+          <p className="italic font-bold text-xl mt-10">verb</p>
+          <p className="meaning text-white-darkest text-xl font-light my-5">
+            Meaning
+          </p>
+          <ul className="meaning-list ml-10 list-disc marker:text-violet">
+            <li className="my-4">To type on a computer keyboard.</li>
+            <p>"Keyboarding is the part of this job I hate the most."</p>
+          </ul>
+        </section>
+      </main>
+      <footer className="flex justify-start gap-10 my-10">
+        <p>Source</p>
+        <a href="https://en.wiktionary.org/wiki/keyboard">
+          https://en.wiktionary.org/wiki/keyboard
+        </a>
+      </footer>
     </div>
   );
 }
--- reactjs_projects/dictionary/tailwind.config.js
+++ reactjs_projects/dictionary/tailwind.config.js
@@ -3,6 +3,24 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: {
+        darkest: "#050505",
+        dark: "#1f1f1f",
+        DEFAULT: "#2d2d2d",
+        light: "#3a3a3a",
+      },
+      white: {
+        darkest: "#757575",
+        dark: "#e9e9e9",
+        light: "#f4f4f4",
+        DEFAULT: "#ffffff",
+      },
+      violet: "#a445ed",
+      red: "#ff5252",
+    },
   },
   plugins: [],
 };
