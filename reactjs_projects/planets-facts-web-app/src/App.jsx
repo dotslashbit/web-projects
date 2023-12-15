84bc479d99fc7694272ebcb18b118c503cd60d36
Sahil Mahapatra <sahilm231199@gmail.com>
2023-12-16 02:07:10 +0530

    rough design covered

--- reactjs_projects/planets-facts-web-app/src/App.jsx
+++ reactjs_projects/planets-facts-web-app/src/App.jsx
@@ -1,7 +1,60 @@
 import { useState } from "react";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="w-full h-screen m-0 p-0 bg-hero bg-darkBlue ">
+      <nav className="flex justify-between items-center m-0 py-5   bg-hero bg-darkBlue text-white">
+        <p className="w-full text-3xl font-antonio">Planets</p>
+        <div className="flex justify-between items-center w-full font-spartan font-thin">
+          <p>Mercury</p>
+          <p>Venus</p>
+          <p>Earth</p>
+          <p>Mars</p>
+          <p>Jupiter</p>
+          <p>Saturn</p>
+          <p>Uranus</p>
+          <p>Neptune</p>
+        </div>
+      </nav>
+      <main className=" text-white">
+        <section className="flex justify-center items-center bg-hero mx-20 p-20">
+          <div className="w-full">
+            <img src="src/assets/planet-earth.svg" alt="planet earth" />
+          </div>
+
+          <div className="flex flex-col justify-between items-start w-full gap-10">
+            <h1 className="font-antonio font-extrabold text-6xl">Earth</h1>
+            <p className="font-spartan font-extralight text-xl">
+              Third planet from the Sun and the only known planet to harbor
+              life. About 29.2% of Earth's surface is land with remaining 70.8%
+              is covered with water. Earth's distance from the Sun, physical
+              properties and geological history have allowed life to evolve and
+              thrive.
+            </p>
+            <a href="#">Source : www.example.com</a>
+          </div>
+        </section>
+      </main>
+      <footer className="flex justify-between items-center text-white w-full mx-20">
+        <div className="flex flex-col justify-between items-start gap-5 w-full ">
+          <p className="text-gray font-spartan">Rotation time</p>
+          <p className="font-antonio text-5xl">0.99 Days</p>
+        </div>
+        <div className="flex flex-col justify-between items-start gap-5 w-full">
+          <p className="text-gray font-spartan">Revolution Time</p>
+          <p className="font-antonio text-5xl">365.26 Days</p>
+        </div>
+        <div className="flex flex-col justify-between items-start gap-5 w-full">
+          <p className="text-gray font-spartan">Radius</p>
+          <p className="font-antonio text-5xl">6,371 KM</p>
+        </div>
+        <div className="flex flex-col justify-between items-start gap-5 w-full">
+          <p className="text-gray font-spartan">Average Temp</p>
+          <p className="font-antonio text-5xl">16°C</p>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- reactjs_projects/planets-facts-web-app/tailwind.config.js
+++ reactjs_projects/planets-facts-web-app/tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#ffffff",
+      darkBlue: "#070724",
+      fadedBlue: "#38384f",
+      gray: "#838391",
+      skyBlue: "#419ebb",
+      yellow: "#eda249",
+      violet: "#6f2ed6",
+      orange: "#d14c32",
+      orangeRed: "#d83a34",
+      fadedOrange: "#cd5120",
+      cyan: "#1ec2a4",
+      blue: "#2d68f0",
+    },
+    fontFamily: {
+      sans: ["Spartan", "sans-serif"],
+      antonio: ["Antonio", "sans-serif"],
+      spartan: ["League Spartan", "sans-serif"],
+    },
+
+    extend: {
+      backgroundImage: {
+        hero: "url('/src/assets/background-stars.svg')",
+      },
+    },
   },
   plugins: [],
 };
